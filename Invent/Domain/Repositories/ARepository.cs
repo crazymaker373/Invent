eb50fa8a6c12eab29e52b0a3aca7d855f9116190
Tobias <t.haidvogl@htlkrems.at>
2022-10-19 15:43:40 +0200

    added db context and arepo, irepo

---
+++ Invent/Domain/Repositories/ARepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Model.Configurations;
+
+namespace Domain.Repositories;
+
+public class ARepository<TEntity>: IRepository<TEntity> where TEntity: class{
+    private readonly StorageDbContext _context;
+    protected readonly DbSet<TEntity> Set;
+
+    public ARepository(StorageDbContext context) {
+        _context = context;
+        Set = _context.Set<TEntity>();
+    }
+
+    public async Task<TEntity?> ReadAsync(int id) => await Set.FindAsync(id);
+
+    public async Task<List<TEntity>> ReadAsync(Expression<Func<TEntity, bool>> filter) => await Set.Where(filter).ToListAsync();
+
+    public async Task<List<TEntity>> ReadAllAsync() => await Set.ToListAsync();
+
+    public async Task<List<TEntity>> ReadAsync(int start, int count) => await Set.Skip(start).Take(count).ToListAsync();
+
+    public async Task<TEntity> CreateAsync(TEntity entity) {
+        Set.Add(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task UpdateAsync(TEntity entity){
+
+        //_context.BlockedPirates.First(e => e.Svnr == "10").Active;
+        
+        _context.ChangeTracker.Clear();
+        Set.Update(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(TEntity entity) {
+        Set.Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+}
