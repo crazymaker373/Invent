0574cc0b6da6be7321e9e6f25d9b8e8ab52307d2
Tobias <t.haidvogl@htlkrems.at>
2022-10-19 16:30:36 +0200

    last commit for today from tobi

--- Invent/Domain/Repositories/LocationRepository.cs
+++ Invent/Domain/Repositories/LocationRepository.cs
@@ -3,7 +3,7 @@ using Model.Entities;
 
 namespace Domain.Repositories;
 
-public class LocationRepository : ARepository<Item>{
+public class LocationRepository : ARepository<Location>{
     public LocationRepository(StorageDbContext context) : base(context){
     }
 }
