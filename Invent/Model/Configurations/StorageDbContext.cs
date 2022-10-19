b1393be3e6e9a00568def5d261857661b25808ca
Marcl_Gengsch <m.genger@htlkrems.at>
2022-10-19 16:10:44 +0200

    [TASK] modelCreating

--- Invent/Model/Configurations/StorageDbContext.cs
+++ Invent/Model/Configurations/StorageDbContext.cs
@@ -13,6 +13,9 @@ public class StorageDbContext : DbContext{
     public DbSet<Location> Locations { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder){
-        
+        builder.Entity<Item>().HasIndex(i => i.Code).IsUnique();
+        builder.Entity<Item>().HasOne(i => i.Location).WithMany().HasForeignKey(i => i.LocationId);
+
+        builder.Entity<Location>().HasOne(i => i.Inventory).WithMany().HasForeignKey(i => i.InventoryId);
     }
 }
