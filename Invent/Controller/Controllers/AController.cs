d27389beacd98aad2d7bed6324c232c859eaa0a1
Tobias <t.haidvogl@htlkrems.at>
2022-10-25 16:23:55 +0200

    added controller

---
+++ Invent/Controller/Controllers/AController.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Repositories;
+
+namespace Controller.Controllers;
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+public abstract class AController<TEntity> : ControllerBase where TEntity : class {
+    private IRepository<TEntity> _repository;
+
+    public AController(IRepository<TEntity> repository) {
+        _repository = repository;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TEntity>> ReadAsync([Required] int id) => Ok(await _repository.ReadAsync(id));
+
+    [HttpGet]
+    public async Task<ActionResult<List<TEntity>>> ReadAllAsync() => Ok(await _repository.ReadAllAsync());
+
+    [HttpPost]
+    public async Task<ActionResult<TEntity>> CreateAsync([Required] TEntity entity) {
+        await _repository.CreateAsync(entity);
+        return NoContent();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<TEntity>> UpdateAsync([Required] int id, [Required] TEntity entity) {
+        var c = await _repository.ReadAsync(id);
+
+        if (c == null) {
+            return NotFound();
+        }
+
+        await _repository.UpdateAsync(entity);
+        return NoContent();
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult<TEntity>> DeleteAsync([Required] int id) {
+        var c = await _repository.ReadAsync(id);
+
+        if (c == null) {
+            return NotFound();
+        }
+
+        await _repository.DeleteAsync(c);
+        return NoContent();
+    }
+}
