33e5de46fec0539e2829e803a43ce8d98877e2df
Marcl_Gengsch <m.genger@htlkrems.at>
2022-11-22 15:46:19 +0100

    migrations

--- Invent/Model/Migrations/20221122141835_LocationAddress.Designer.cs
+++ Invent/Model/Migrations/20221122141835_LocationAddress.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Model.Configurations;
 
@@ -10,9 +11,10 @@ using Model.Configurations;
 namespace Model.Migrations
 {
     [DbContext(typeof(StorageDbContext))]
-    partial class StorageDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221122141835_LocationAddress")]
+    partial class LocationAddress
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
