84265b1b7664990d9396c9df826b1bf6d960c16a
Marcl_Gengsch <m.genger@htlkrems.at>
2022-12-21 14:24:12 +0100

    [TASK] delete view for Controller

--- Invent/Controller/Program.cs
+++ Invent/Controller/Program.cs
@@ -1,9 +1,15 @@
 using Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Model.Configurations;
 using Model.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<StorageDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
