2e71f7637e8438c66c3c89b6abc931fc4232871a
Marcl_Gengsch <m.genger@htlkrems.at>
2022-12-21 13:55:48 +0100

    [TASK] add controller

---
+++ Invent/Controller/Controllers/InventoryController.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Model.Entities;
+
+namespace Controller.Controllers;
+
+[ApiController]
+[Route("inventory")]
+public class InventoryController : AController<Inventory> {
+    public InventoryController(IRepository<Inventory> repository) : base(repository) {
+    }
+}
---
+++ Invent/Controller/Controllers/LocationController.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Model.Entities;
+
+namespace Controller.Controllers;
+
+[ApiController]
+[Route("location")]
+public class LocationController : AController<Location> {
+    public LocationController(IRepository<Location> repository) : base(repository) {
+    }
+}
